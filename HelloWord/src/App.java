5216633eb735f454d3c470a22544022e74ed6ea1
Ythalo Dutra <ydutra934@gmail.com>
2024-09-04 21:41:17 -0300

    Primeiro e Segundo nome

--- HelloWord/src/App.java
+++ HelloWord/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Java GitHub!!");
+    public static void main(String[] args) {
+        System.out.println(nomeCompleto("Ythalo", "Pereira"));
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do Método:" + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
+
